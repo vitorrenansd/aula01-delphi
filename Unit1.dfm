1f071a7003082db5dbbf46698f25ea715cbdafd5
vitorrenansouzadias@gmail.com <Vitor>
2025-08-20 15:53:51 -0300

    Initial commit

---
+++ Unit1.dfm
@@ -0,0 +1,31 @@
+object Form1: TForm1
+  Left = 0
+  Top = 0
+  Caption = 'Form1'
+  ClientHeight = 169
+  ClientWidth = 425
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -12
+  Font.Name = 'Segoe UI'
+  Font.Style = []
+  TextHeight = 15
+  object Button1: TButton
+    Left = 166
+    Top = 105
+    Width = 75
+    Height = 25
+    Caption = 'Button1'
+    TabOrder = 0
+    OnClick = Button1Click
+  end
+  object Edit1: TEdit
+    Left = 88
+    Top = 48
+    Width = 233
+    Height = 23
+    TabOrder = 1
+    Text = 'Edit1'
+  end
+end
---
+++ Unit1.pas
@@ -0,0 +1,37 @@
+unit Unit1;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls;
+
+type
+  TForm1 = class(TForm)
+    Button1: TButton;
+    Edit1: TEdit;
+    procedure Button1Click(Sender: TObject);
+  private
+    id : integer;
+    description : string;
+    price : currency;
+    active : boolean;
+  public
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  id := 1;
+  description := 'Mouse generico';
+  price := 35.99;
+  active := true;
+end;
+
+end.
